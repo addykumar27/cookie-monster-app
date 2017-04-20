c0eecc7113f9e81c399c6471419e64d844bc6a48
jeanmw <jean.weatherwax@gmail.com>
2017-04-20 10:18:51 -0700

    fixing routes and setup for starter code

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  root 'desserts#index'
+  get '/desserts' => 'desserts#index'
+  get '/desserts/favorite' => 'desserts#favorite', as: :favorite_dessert
+  get '/desserts/:id' => 'desserts#show', as: :show_dessert
+  post '/desserts/:id/save_as_favorite' => 'desserts#save_as_favorite', as: :save_as_favorite
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
