24d76686201bcbc48ab377e30da81e51f591a420
addykumar27 <adarshna27@yahoo.com>
2017-05-23 15:56:47 -0700

    comp till challenge three

--- app/controllers/desserts_controller.rb
+++ app/controllers/desserts_controller.rb
@@ -3,11 +3,14 @@ class DessertsController < ApplicationController
   # GET /desserts
   def index
     @desserts = Dessert.all
+    cookies[:can_access] = "yeah"
   end
 
   # GET /desserts/1
   def show
     redirect_to root_path and return if not is_logged_in
+    
+
 
     ## CHALLENGE 4: This is my favorite dessert!
     ### Detect whether the dessert the user is viewing
@@ -15,18 +18,14 @@ class DessertsController < ApplicationController
     ### 'FAVORITE dessert' in the `views/desserts/show.html.erb` template
 
     @dessert = Dessert.find(params[:id])
+    
   end
 
   # GET /desserts/favorite
   # Shows the user's favorite dessert (if set in the session)
   def favorite
     redirect_to root_path and return if not is_logged_in
-
-    ## CHALLENGE 3: Render my favorite_dessert!
-    ### Check my session for my `favorite_dessert_id`
-    ### and then render my favorite dessert
-    ### AS-IS this will raise an error
-    favorite_dessert_id = 34404 # CHANGE THIS
+    favorite_dessert_id = session[:favorite_dessert_id]
     @dessert = Dessert.find(favorite_dessert_id)
     render 'show'
   end
@@ -35,7 +34,8 @@ class DessertsController < ApplicationController
   # Sets the user's favorite dessert in their session
   def save_as_favorite
     redirect_to root_path and return if not is_logged_in
-    session[:favorite_dessert_id] = params[:id] # SET favorite
+    session[:favorite_dessert_id] = params[:id]
+     # SET favorite
     flash[:notice] = "dessert #{params[:id]} saved in session as favorite!"
     redirect_to root_path
   end
